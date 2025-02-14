27be541ffd62136ded58fa3292e1beb2ce5eeb7a
Ronny Manuel De la Cruz Perez <62533639+DeLaCruz-Ronny@users.noreply.github.com>
2025-02-14 08:02:43 -0400

    Agregar archivos de proyecto.

---
+++ Models/GetWeather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consumir_api.Models
+{
+    public class GetWeather
+    {
+        public DateOnly date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF => 32 + (int)(temperatureC / 0.5556);
+        public string ? summary { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using consumir_api.Models;
+using System.Text.Json;
+
+namespace consumir_api
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string apiUrl = "https://webapplication1-production.up.railway.app/WeatherForecast";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        // Deserializar la respuesta JSON a un array de objetos
+                        var get = JsonSerializer.Deserialize<GetWeather[]>(responseData);
+
+                        foreach (var data in get)
+                        {
+                            Console.WriteLine($"Fecha: {data.date}, Celsius: {data.temperatureC}, Farenheint: {data.temperatureF}, Zona: {data.summary}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode}");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Error en la solicitud: {e.Message}");
+                }
+            }
+        }
+    }
+}
